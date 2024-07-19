bef20cac2b732f918e1fe5df011bd2a29fcf7d0a
maarceloBjr <maarcelo.bjr@gmail.com>
2024-07-19 18:58:21 -0300

    fix: add level requirement for report

--- src/routes/report.ts
+++ src/routes/report.ts
@@ -8,7 +8,15 @@ if (!SECRET_KEY) {
   throw new Error("A variável de ambiente SECRET_KEY_JWT não está definida");
 }
 
-// Middleware para verificar o token JWT
+// Defina a interface do usuário que inclui o nível
+interface IUser extends JwtPayload {
+  name: string;
+  email: string;
+  password: string;
+  level: 1 | 2 | 3 | 4 | 5;
+}
+
+// Middleware para autenticação do token e verificação do nível do usuário
 const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
@@ -18,11 +26,17 @@ const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   jwt.verify(token, SECRET_KEY, (err, user) => {
     if (err) return res.status(403).json({ message: "Token inválido" });
 
-    req.user = user as JwtPayload;
+    const authenticatedUser = user as IUser;
+    if (authenticatedUser.level < 4) {
+      return res.status(403).json({ message: "Nível de usuário insuficiente" });
+    }
+
+    req.user = authenticatedUser;
     next();
   });
 };
 
+// Endpoint para imprimir o relatório
 router.get("/report", authenticateToken, async (req: Request, res: Response) => {
   try {
     res.json({ message: "Relatório impresso" });
