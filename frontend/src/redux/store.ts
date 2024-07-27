bf650280890f61974858a782898d5f0d0fa8645d
maarceloBjr <maarcelo.bjr@gmail.com>
2024-07-27 20:26:25 -0300

    fix: adjusted token local storage

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,6 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "../pages/users/store/reducers";
 import authReducer, { login } from "../pages/login/store/reducers";
+import { JwtPayload, jwtDecode } from "jwt-decode";
+
+interface DecodedToken extends JwtPayload {
+  exp: number;
+}
 
 export const store = configureStore({
   reducer: {
@@ -9,11 +14,28 @@ export const store = configureStore({
   },
 });
 
-const token = localStorage.getItem("token");
-if (token) {
-  store.dispatch(login(token));
-}
+const isTokenValid = (token: string | null): boolean => {
+  if (!token) return false;
 
+  try {
+    const decodedToken = jwtDecode<DecodedToken>(token);
+    const currentTime = Date.now() / 1000;
+
+    return decodedToken.exp > currentTime;
+  } catch (error) {
+    return false;
+  }
+};
+
+const token = localStorage.getItem("token");
+
+console.log("token", token);
+
+if (token && isTokenValid(token)) {
+  store.dispatch(login(token));
+}else{
+    localStorage.removeItem("token");
+}
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
